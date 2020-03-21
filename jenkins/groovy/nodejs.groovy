a6460a900b1d9fc38e0416d4c031e24aa4bf5795
Apostolof <apotwohd@gmail.com>
2020-03-21 19:55:47 +0200

    Fix jobs copying, Add nodeJS v12

--- jenkins/groovy/nodejs.groovy
+++ jenkins/groovy/nodejs.groovy
@@ -6,10 +6,11 @@ import jenkins.plugins.nodejs.tools.NodeJSInstaller
 import static jenkins.plugins.nodejs.tools.NodeJSInstaller.DEFAULT_NPM_PACKAGES_REFRESH_HOURS
 
 final versions = [
-        'NodeJS 13.x': '13.10.1'
+        'NodeJS 13.x': '13.10.1',
+        'NodeJS 12.x': '12.16.1'
 ]
 
-final globalPackages = "eslint@~6.8.0"
+final globalPackages = "eslint@~6.8.0 truffle@~5.1.18"
 
 Jenkins.instance.getDescriptor(NodeJSInstallation).with {
     installations = versions.collect {
